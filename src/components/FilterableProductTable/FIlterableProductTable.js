f6e5b9d4a19d9424c6b74a220e9938e3e4008f00
VietKhang <1511465@hcmut.edu.vn>
2018-06-22 13:53:00 +0700

    Upload

--- src/components/FilterableProductTable/FIlterableProductTable.js
+++ src/components/FilterableProductTable/FIlterableProductTable.js
@@ -1,6 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ProductTable from '../ProductTable/ProductTable';
 import SearchBar from '../SearchBar/SearchBar';
-const filterProductTable = (props) => (
-    <SearchBar />
-)
-export default filterProductTable;
+class FilterProductTable extends Component {
+  render() {
+    const PRODUCTS = [
+      {
+        category: 'Sporting Goods',
+        price: '$49.99',
+        stocked: true,
+        name: 'Football'
+      },
+      {
+        category: 'Sporting Goods',
+        price: '$9.99',
+        stocked: true,
+        name: 'Baseball'
+      },
+      {
+        category: 'Sporting Goods',
+        price: '$29.99',
+        stocked: false,
+        name: 'Basketball'
+      },
+      {
+        category: 'Electronics',
+        price: '$99.99',
+        stocked: true,
+        name: 'iPod Touch'
+      },
+      {
+        category: 'Electronics',
+        price: '$399.99',
+        stocked: false,
+        name: 'iPhone 5'
+      },
+      {
+        category: 'Electronics',
+        price: '$199.99',
+        stocked: true,
+        name: 'Nexus 7'
+      }
+    ];
+    return (
+      <div>
+        <SearchBar />
+        <ProductTable  products = {PRODUCTS}/>
+      </div>
+    );
+  }
+}
+export default FilterProductTable;
